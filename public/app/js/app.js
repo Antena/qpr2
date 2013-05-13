73794658756fd4d702efef42f9415ad9a59e1dc9
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-05-13 12:12:09 -0300

    Summy data

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -4,6 +4,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('qpr2', ['qpr2.filters', 'qpr2.services', 'qpr2.directives', 'qpr2.controllers']).
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'partials/entities.html', controller: 'StoryController'});
+        $routeProvider.when('/', {templateUrl: 'partials/story.html', controller: 'StoryController'});
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -5,16 +5,32 @@
 angular.module('qpr2.controllers', []).
     controller('StoryController', ['$scope', function($scope) {
         $scope.articles = [
-            { title: "Noticia 1", age: 0 },
-            { title: "Noticia 2", age: 1 }
+            {
+                link: "http://www.argentina.ar/temas/pais/17693-aysa-a-7-anos-de-su-reestatizacion-avanza-con-su-plan-director",
+                title: "AYSA: a 7 años de su reestatización, avanza con su plan director",
+                date: "21/03/2013",
+                description: "La empresa tiene previsto para el corriente año una inversión de 6.000 millones de pesos para finalizar cuatro obras de envergadura para la mejor prestación de servicio, en el marco de su plan director."
+            },
+            {
+                link: "http://noticias.terra.com.ar/acuerdo-de-aysa-y-corporacion-andina-para-capacitar-cooperativas,7f272138a97bf210VgnVCM4000009bf154d0RCRD.html",
+                title: "ACUERDO DE AYSA Y CORPORACION ANDINA PARA CAPACITAR COOPERATIVAS",
+                date: "03/05/2011",
+                description: "La Corporación Andina de Fomento (CAF) y la empresa Aguas y Saneamientos Argentinos Sociedad Anónima (AYSA) firmaron un convenio que le permitirá a la compañía estatal recibir fondos no reembolsables para la conformación y capacitación de cooperativas de trabajo, que beneficará a más de 1500 familias."
+            }
         ];
 
         $scope.reports = [
             { id: "66" }
         ]
 
+        $scope.topics = [
+            { name: "Planta Potabilizadora", labelClass: "label-info" },
+            { name: "Obras Inconclusas", labelClass: "label-important" }
+        ]
+
         $scope.story = {
-            title: "Nombre del hotspot"
+            title: "Planta potabilizadora Almirante Brown",
+            description: "En el taller de Monitoreo Social de la Cuenca Matanza-Riachuelo realizado el 25 de febrero de 2013 en la localidad de Almirante Brown, se reportó que se encontraban inconclusas las obras de una planta potabilizadora sita en Avda. Juan B. Justo y Virrey Ceballos, localidad de Glew, Partido de Almirante Brown, Buenos Aires. Esta situación traería como consecuencia la restricción al acceso a agua potable por parte de los vecinos de dicho lugar."
         };
     }])
     .controller('MyCtrl2', [function() {
